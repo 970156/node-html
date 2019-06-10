0c43d63e7c68cd9a921fa0b1639f73404e2014ff
mounikaroyal <mounika@stucred.com>
2019-06-10 11:26:32 +0530

    new
    Signed-off-by: mounikaroyal <mounika@stucred.com>

--- 1json.js
+++ 1json.js
@@ -1,9 +1,9 @@
-// const name = ('john') 
-// console.warn(`do not track ${name}! do not track! `);
-// console.log("hello world")
-// //console.error(new Error('helthis is wrong method.'))
-// console.error(new Error('Hell! This is a wrong method.'));  
-//console.log(__filename);
+const name = ('john') 
+console.warn(`do not track ${name}! do not track! `);
+console.log("hello world")
+//console.error(new Error('helthis is wrong method.'))
+console.error(new Error('Hell! This is a wrong method.'));  
+console.log(__filename);
 
 var fs = require("fs");  
 var data = 'this is the new node.js created by me in this session  ';  
